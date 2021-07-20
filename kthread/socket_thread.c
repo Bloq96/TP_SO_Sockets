c8412937d38dd0ace7bd2737b3da60e1c8a905f9
Debian <debian@debian-hostname.bloq96.net>
2021-07-20 01:35:12 -0300

    Updates to socket_thread.c

--- kthread/socket_thread.c
+++ kthread/socket_thread.c
@@ -8,87 +8,114 @@
 #include <linux/socket.h>
 //#include <linux/types.h>
 #include <linux/uaccess.h>
+#include <linux/uio.h>
 //#include <linux/unistd.h>
 //#include <netdb.h>
 
 #define MAX_PENDING 5
 #define MESSAGE_MAX_LENGTH 256
-#define SERVER_PORT 54321
+#define SOCKET_PORT 54321
 
 static struct task_struct *threadInfo;
 
 static int socketServer(void *unused) {
-    char buffer[MESSAGE_MAX_LENGTH];
-    unsigned int addressLenght = sizeof(socketAddress);
+    char outputBuffer[MESSAGE_MAX_LENGTH],
+    inputBuffer[MESSAGE_MAX_LENGTH];
     unsigned int messageLenght;
-    struct socket *defaultSocket;
-    int err;
+    struct socket *defaultSocket, *newConnectionSocket;
+    struct sockaddr_in socketAddress;
+    int err = -1;
+    struct msghdr outputMessage, inputMessage;
+    struct iovec iovW[1], iovR[1];
 
     printk(KERN_INFO "Starting socket server...\n");
     printk(KERN_INFO "Kernel thread created as: %s [PID=%d]\n",
     current->comm,current->pid);
 
-    err = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &defaultSocket);
+    err = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP,
+    &defaultSocket);
     if (err < 0) {
         printk(KERN_INFO "Error: Could not create socket.\n");
         return -1;
     }
-    if ((bind(defaultSocket, (struct sockaddr *)&defaultSocket,
-    sizeof(defaultSocket))) < 0) {
-        printk(KERN_INFO  "Error: Could not bind socket to address.\n");
+   
+    memset(&socketAddress,0, sizeof(socketAddress));
+    socketAddress.sin_family = AF_INET;
+    socketAddress.sin_port = htons(SOCKET_PORT);
+    socketAddress.sin_addr.s_addr = INADDR_ANY;
+
+    err = defaultSocket->ops->bind(defaultSocket,
+    (struct sockaddr *)&socketAddress,sizeof (socketAddress));
+    if (err < 0) {
+        printk(KERN_INFO "Error: Could not bind socket to address.\n");
         return -1;
     }
-    listen(defaultSocket, MAX_PENDING);
-
-    //while(1) {
-        printk(KERN_INFO "Aguardando conexão do client.\n");
-        if ((newConnectionSocket = accept(defaultSocket,
-	(struct sockaddr *)&defaultSocket, &addressLenght)) < 0) {
-            printk(KERN_INFO "Error: Could not connect to client.\n");
-            return -1;
-        }
-        printk(KERN_INFO "Connection established.\n");
-	messageLenght = 14;
-        buffer[0] = 'H'; buffer[1] = 'e'; buffer[2] = 'l';
-       	buffer[3] = 'l'; buffer[4] = 'o'; buffer[5] = ',';
-	buffer[6] = ' '; buffer[7] = 'w'; buffer[8] = 'o';
-	buffer[9] = 'r'; buffer[10] = 'l'; buffer[11] = 'd';
-        buffer[12] = '!'; buffer[13] = '\n';	
-        send(newConnectionSocket, buffer, messageLenght, 0);
-        printk(KERN_INFO "Message sent.\n");
-	while(recv(newConnectionSocket, buffer, sizeof(buffer),0));
-        close(newConnectionSocket);
-        printk(KERN_INFO "Client disconnected.\n");
-        sock_release(defaultSocket);
-        printk(KERN_INFO "Socket connection closed.\n");
-	return 0;
-    //}
-}
-
-static int printLine(void *unused) {
-    struct file *outputFile;
-    char string[13] = {'H','e','l','l','o',',','w','o','r','l','d',
-    '!','\n'};
-    loff_t pos = 0;
-    printk(KERN_INFO "Dummy thread starting...\n");
-    printk(KERN_INFO "Kernel thread created as: %s [PID=%d]",
-    current->comm,current->pid);
-    printk(KERN_INFO "Input string: %s",string);
-    outputFile = filp_open("/home/debian/Documents/kthread/output.txt",
-    O_WRONLY|O_CREAT,0666);
-    if(IS_ERR(outputFile)) {
-        printk(KERN_INFO "Error opening the file!");
-	return -1;
+    err = defaultSocket->ops->listen(defaultSocket, MAX_PENDING);
+    if (err < 0) {
+        printk(KERN_INFO "Error: Could not listen to socket.\n");
+        return -1;
     }
-    kernel_write(outputFile,string,13,&pos);
-    filp_close(outputFile,NULL);
-    printk(KERN_INFO "Dummy thread stopping...\n");
+
+    printk(KERN_INFO "Aguardando conexão do client.\n");
+
+    newConnectionSocket = sock_alloc();
+    newConnectionSocket->type = defaultSocket->type;
+    newConnectionSocket->ops = defaultSocket->ops;
+    err = defaultSocket->ops->accept(defaultSocket,
+    newConnectionSocket, 0, false);
+    if (err < 0) {
+        printk(KERN_INFO "Error: Could not connect.\n");
+        return -1;
+    }
+
+    printk(KERN_INFO "Connection established.\n");
+  
+    messageLenght = 14;
+    outputBuffer[0] = 'H'; outputBuffer[1] = 'e';
+    outputBuffer[2] = 'l'; outputBuffer[3] = 'l';
+    outputBuffer[4] = 'o'; outputBuffer[5] = ',';
+    outputBuffer[6] = ' '; outputBuffer[7] = 'w';
+    outputBuffer[8] = 'o'; outputBuffer[9] = 'r';
+    outputBuffer[10] = 'l'; outputBuffer[11] = 'd';
+    outputBuffer[12] = '!'; outputBuffer[13] = '\n';
+
+    outputMessage.msg_name = NULL;
+    outputMessage.msg_namelen = 0;
+    outputMessage.msg_control = NULL;
+    outputMessage.msg_controllen = 0;
+    outputMessage.msg_flags = MSG_DONTWAIT;
+    outputMessage.msg_iocb = NULL;
+    iovW[0].iov_len = messageLenght;
+    iovW[0].iov_base = (char *) outputBuffer;
+    iov_iter_init(&outputMessage.msg_iter, WRITE, iovW, 1,
+    messageLenght);
+    
+    sock_sendmsg(newConnectionSocket, &outputMessage);
+    printk(KERN_INFO "Message sent.\n");
+
+    inputMessage.msg_name = NULL;
+    inputMessage.msg_namelen = 0;
+    inputMessage.msg_control = NULL;
+    inputMessage.msg_controllen = 0;
+    inputMessage.msg_flags = MSG_DONTWAIT;
+    inputMessage.msg_iocb = NULL;
+    iovR[0].iov_len = MESSAGE_MAX_LENGTH;
+    iovR[0].iov_base = (char *) inputBuffer;
+    iov_iter_init(&inputMessage.msg_iter, READ, iovR, 1,
+    MESSAGE_MAX_LENGTH);
+
+    while(sock_recvmsg(newConnectionSocket, &inputMessage, 0));
+    printk(KERN_INFO "Message sent.\n");
+    sock_release(newConnectionSocket);
+    printk(KERN_INFO "Client disconnected.\n");
+    sock_release(defaultSocket);
+    printk(KERN_INFO "Socket connection closed.\n");
     return 0;
 }
 
 static int __init initThread(void) {
     printk(KERN_INFO "Dummy thread creation\n");
-    threadInfo = kthread_create(printLine,NULL,"Dummy Thread 1");
+    threadInfo = kthread_create(socketServer,NULL,"Dummy Thread 1");
     if(IS_ERR(threadInfo)) {
         printk(KERN_INFO "Failed creating the thread.");
     } else {
